eaed1ebb1c7d7b3db6fb2921df9ca91d61eaf758
yjn <uje1000@gmail.com>
2025-07-22 09:25:32 +0900

    addhw

---
+++ Python/SSAFY_hw/python_ws_1_3/ws_1_3.py
@@ -0,0 +1,12 @@
+# main.py
+
+twinkle = '반짝 반짝'
+place = '에서도'
+little_star = '작은별'
+wonder = '아름답게 비치네'
+east_sky = '동쪽 하늘'
+west_sky = '서쪽 하늘'
+
+print(f'{twinkle} {little_star} {wonder}')
+print(f'{east_sky} {place} {west_sky} {place}')
+print(f'{twinkle} {little_star} {wonder}')
